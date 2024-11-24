8a04b85bcf9f4115eb78b622c89e16b1ff7d6566
Yashverma <yv3245650@gmail.comgit config --global user.name YashVerma>
2024-11-24 23:56:46 +0530

    Array

---
+++ ArrayJava.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+// import java.util.*;
+public class ArrayJava {
+    public static void main(String[] args){
+        int [] arr={1,2,3,4,5};
+      
+    //  }
+        // Scanner in=new Scanner(System.in);
+        // int [] arr= new int[5];
+     
+        // First Way to print
+        // for(int i =0;i<arr.length;i++){
+          
+        // }
+        // SEcond way to print
+        // for(int i=0;i<arr.length;i++){
+        //     arr[i]=in.nextInt();
+
+        // }
+        // System.out.println(Arrays.toString(arr));
+     for(int num:arr){
+    //     change(num);
+        System.out.print(num+" "); 
+    //  }
+
+        }
+        
+        
+    }
+}
+
